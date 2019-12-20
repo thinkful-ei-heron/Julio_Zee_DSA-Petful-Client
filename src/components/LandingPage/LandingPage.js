7cb00f6df16a3ad8c056166c985b344d64f625e2
julio hernandez <hernandezjulio58@ymail.com>
2019-12-20 11:55:45 -0800

    organizing files, currently have landing page/adoption page being worked on

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello, welcome to out adoption website !</h3>
+        <p>
+          Below we have our current line up of dogs and cats that are up for
+          adoption. Get in line now and adopt a beautiful little fluff ball.
+        </p>
+      </div>
+    );
+  }
+}
---
+++ src/routes/landingRoute/landingRoute.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LandingPage from '../../components/LandingPage/LandingPage';
+
+export default class landingRoute extends Component {
+  render() {
+    return (
+      <div>
+        <LandingPage />
+      </div>
+    );
+  }
+}
